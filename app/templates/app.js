611d7fe00d6c4c4cca481ea16d597b19c4ef9855
Christoph Böhner-Figas <boehner@team23.de>
2018-02-19 22:50:47 +0100

    Port latest script setup

--- app/templates/app.js
+++ app/templates/app.js
@@ -9,42 +9,69 @@ requirejs(['require',
     function(require, Flickity) {
         'use strict';
 
-    var font = new FontFaceObserver('Fira Sans');
-    font.load().then(function () {
-        document.documentElement.className += " app-fonts-loaded";
-    });
-    var $bannerBar = document.querySelectorAll('.app-js-carousel'),
-        $galleryContainer = document.querySelectorAll('.js-gallery');
-    if ($bannerBar.length) {
-        var bannerflkty = new Flickity('.app-js-carousel', {
-            autoPlay: 7000,
-            contain: true,
-            wrapAround: true,
-            imagesLoaded: true,
-            cellSelector: '.app-banner-item',
-            cellAlign: 'left',
-            selectedAttraction: 0.025,
-            friction: 0.28
-        });
-    }
-    // Content image galleries
-    if ($galleryContainer.length) {
-        var flkty = new Flickity('.js-gallery', {
-            autoPlay: true,
-            contain: true,
-            wrapAround: true,
-            imagesLoaded: true,
-            cellSelector: '.app-gallery-cell',
-            cellAlign: 'left'
-        });
-    }
-    // Anonymous only scripts (mainly used in login views)
-    if ($(".userrole-anonymous")[0]) {
-        // Show password by default
-        $('input[type="password"]').hideShowPassword(true, 'focus');
-        //$('input[type="password"]').showPassword('focus', {});
-        // Float labels (requires corresponding css)
-        $('.app-signin-input').jvFloat();
+        if (typeof a25 == 'undefined') {
+            var a25 = {};
+        }
 
-    };
-});
+
+        // Trigger font face observer protection
+        var fontPrimary = new FontFaceObserver('Raleway');
+        var fontSecondary = new FontFaceObserver('Special Elite');
+
+        fontPrimary.load().then(function () {
+            document.documentElement.className += " font__primary--loaded";
+        });
+
+        fontSecondary.load().then(function () {
+            document.documentElement.className += " font__secondary--loaded";
+        });
+
+        Promise.all([fontPrimary.load(), fontSecondary.load()]).then(function () {
+            document.documentElement.className += " fonts--loaded";
+        });
+
+        var $bannerBar = document.querySelector('.app-js-carousel'),
+            $galleryContainer = document.querySelector('.js-gallery');
+        if ($bannerBar !== null) {
+            var bannerflkty = new Flickity('.app-js-carousel', {
+                pauseAutoPlayOnHover: false,
+                autoPlay: 7000,
+                contain: true,
+                wrapAround: true,
+                imagesLoaded: true,
+                cellSelector: '.app-banner-item',
+                cellAlign: 'left',
+                selectedAttraction: 0.025,
+                friction: 0.28
+            });
+            $bannerBar.classList.add('app-banner--loaded');
+        }
+        // Content image galleries
+        if ($galleryContainer !== null) {
+            var flkty = new Flickity('.js-gallery', {
+                autoPlay: true,
+                contain: true,
+                wrapAround: true,
+                imagesLoaded: true,
+                cellSelector: '.app-gallery-cell',
+                cellAlign: 'left'
+            });
+            $galleryContainer.classList.add('app-banner--loaded');
+        }
+
+        // Initialize scripts
+
+        // Load Slider Resize
+        window.addEventListener('load', function() {
+            var sliders = Array.prototype.slice.call(document.querySelectorAll('.js-slider-resize'));
+            if (sliders) {
+                sliders.forEach(function(slider) {
+                    var flkty = Flickity.data(slider);
+                    flkty.resize()
+                });
+            }
+        });
+
+
+
+    });
